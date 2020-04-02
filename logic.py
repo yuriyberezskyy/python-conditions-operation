31a58f2da0bbe40a9dc9bcebb2e278fc1cc9a148
yuriyberezskyy <bereza95@gmail.com>
2020-04-02 16:16:17 -0400

    done

---
+++ logic.py
@@ -0,0 +1,13 @@
+name = " "
+if not name.strip():
+    print("There is no name")
+else:
+    print(f"My name is {name}")
+
+
+age = 17
+
+if 18 <= age < 40:
+    print("It's allowed to drink for you sir")
+else:
+    print("Not allowed")
